dd13c09df2628fa85d4d6e76d0aecf33cc345dd0
Shai <amsalemshay@gmail.com>
2016-06-06 12:18:50 +0300

    Made the camera follow the player

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public RectTransform Player;
+
+    public Vector2 Margin; // How many steps the player can move until the camera corrects it's position
+    public Vector2 Smoothing;
+
+    public BoxCollider2D Bounds;
+
+    private Vector3 _min, _max;
+
+    public bool IsFollowing { get; set; }
+
+	void Start () {
+        _min = Bounds.bounds.min;
+        _max = Bounds.bounds.max;
+
+        
+        IsFollowing = true;
+    }
+	
+	void Update () {
+        var x = transform.position.x;
+        var y = transform.position.y;
+
+        if (IsFollowing)
+        {
+            // If the camera is not within the margin we set - we need to move it 
+            if (Mathf.Abs(x - Player.position.x) > Margin.x) x = Mathf.Lerp(x, Player.anchoredPosition.x, Smoothing.x * Time.deltaTime);
+            if (Mathf.Abs(y - Player.position.y) > Margin.y) y = Mathf.Lerp(y, Player.anchoredPosition.y, Smoothing.y * Time.deltaTime);
+        }
+
+        var cameraHalfWidth = Camera.main.orthographicSize * ((float)Screen.width / Screen.height);
+
+        x = Mathf.Clamp(x, _min.x + cameraHalfWidth, _max.x - cameraHalfWidth);
+        y = Mathf.Clamp(y, _min.y + Camera.main.orthographicSize, _max.y - Camera.main.orthographicSize);
+
+        transform.position = new Vector3(x, y, transform.position.z);
+    }
+}
