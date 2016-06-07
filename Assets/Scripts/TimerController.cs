b5784de26372798a8dd833b436e96f8602379591
Shai <amsalemshay@gmail.com>
2016-06-07 11:15:12 +0300

    Added basic support for timer

---
+++ Assets/Scripts/TimerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+
+public class TimerController : MonoBehaviour {
+
+    public TimeSpan LevelTimer { get; set; }
+    private Text _timerText;
+
+
+	// Use this for initialization
+	void Start () {
+        LevelTimer = TimeSpan.FromMinutes(6);
+        _timerText = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        LevelTimer = LevelTimer.Subtract(TimeSpan.FromSeconds(Time.deltaTime));
+        _timerText.text = LevelTimer.ToString();
+	}
+}
