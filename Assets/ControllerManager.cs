9bce5c918c005d986c4e738be7a5f4db63b05d8f
iLnF <zvulun.aviv@gmail.com>
2016-06-12 20:26:22 +0300

    Added Basic Level + Added Windows Controller Support

--- Assets/ControllerManager.cs
+++ Assets/ControllerManager.cs
@@ -6,13 +6,13 @@ using Assets.Scripts;
 
 public class ControllerManager : ContollerBaseMonoBehavior
 {
-    public float MovingSpeed = 2;
+    public float MovingSpeed = 6;
     public float JumpingForce = 20000;
     public LayerMask[] groundLayers = new LayerMask[0];
 
     private Vector2 _stepsToMove;
     private Rigidbody2D _playerRigidBody;
-    
+
     override protected void Start()
     {
         base.Start();
@@ -42,7 +42,28 @@ public class ControllerManager : ContollerBaseMonoBehavior
     void Update()
     {
         MovePlayer(_stepsToMove);
+
     }
+
+	// Added Windows controller support - Aviv
+	void FixedUpdate ()
+	{
+
+		// Character Jump
+		if (Input.GetKeyDown ("space")){
+			if (CanJump()) _playerRigidBody.AddForce(new Vector2(0, JumpingForce));
+		} 
+
+		// Character fire
+	
+		// Move Character left and right
+		if (Input.GetKey (KeyCode.RightArrow) || Input.GetKey (KeyCode.D)) {
+			Player.transform.Translate (Vector3.right * MovingSpeed);
+		} else if (Input.GetKey (KeyCode.LeftArrow) || Input.GetKey (KeyCode.A)) {
+			Player.transform.Translate (Vector3.right * -MovingSpeed);
+		}
+			
+	}
 
     private void MovePlayer(Vector2 pMovement)
     {
