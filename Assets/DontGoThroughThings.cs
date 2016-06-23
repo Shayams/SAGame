a9a8d3b7c27bb5c10d3bb9351122a80f83779034
Shai <amsalemshay@gmail.com>
2016-06-23 17:32:02 +0300

    Added a script that prevents the player from going though the bounds

---
+++ Assets/DontGoThroughThings.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DontGoThroughThings : MonoBehaviour {
+
+    // Careful when setting this to true - it might cause double
+    // events to be fired - but it won't pass through the trigger
+    public bool sendTriggerMessage = false;
+
+    public LayerMask layerMask = -1; //make sure we aren't in this layer 
+    public float skinWidth = 0.1f; //probably doesn't need to be changed 
+
+    private float minimumExtent;
+    private float partialExtent;
+    private float sqrMinimumExtent;
+    private Vector2 previousPosition;
+    private Rigidbody2D myRigidbody;
+    private Collider2D myCollider;
+
+    //initialize values 
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myCollider = GetComponent<Collider2D>();
+        previousPosition = myRigidbody.position;
+        minimumExtent = Mathf.Min(Mathf.Min(myCollider.bounds.extents.x, myCollider.bounds.extents.y), myCollider.bounds.extents.z);
+        partialExtent = minimumExtent * (1.0f - skinWidth);
+        sqrMinimumExtent = minimumExtent * minimumExtent;
+    }
+
+    void FixedUpdate()
+    {
+        //have we moved more than our minimum extent? 
+        Vector2 movementThisStep = myRigidbody.position - previousPosition;
+        float movementSqrMagnitude = movementThisStep.sqrMagnitude;
+
+        if (movementSqrMagnitude > sqrMinimumExtent)
+        {
+            float movementMagnitude = Mathf.Sqrt(movementSqrMagnitude);
+
+            //check for obstructions we might have missed 
+            RaycastHit2D hitInfo = Physics2D.Raycast(previousPosition, movementThisStep, movementMagnitude, layerMask.value, 0.1f);
+            if (true)
+            {
+                if (!hitInfo.collider)
+                    return;
+
+                if (hitInfo.collider.isTrigger)
+                    hitInfo.collider.SendMessage("OnTriggerEnter2D", myCollider);
+
+                if (!hitInfo.collider.isTrigger)
+                {
+                    var rotationExp = (int)transform.rotation.y == 0 ? 1 : -1;
+                    myRigidbody.position = new Vector2(hitInfo.point.x - (20*rotationExp) - (movementThisStep / movementMagnitude).x * partialExtent, myRigidbody.position.y);
+                }
+
+            }
+        }
+
+        previousPosition = myRigidbody.position;
+    }
+}
