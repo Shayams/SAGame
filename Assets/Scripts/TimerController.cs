efca32ac5b6c9dc0ec6dd7c0117d3685cbe1baea
Shai <amsalemshay@gmail.com>
2016-07-03 23:47:39 +0300

    Changed the timer to RX

--- Assets/Scripts/TimerController.cs
+++ Assets/Scripts/TimerController.cs
@@ -2,22 +2,30 @@
 using UnityEngine.UI;
 using System.Collections;
 using System;
+using UniRx;
 
 public class TimerController : MonoBehaviour {
 
-    public TimeSpan LevelTimer { get; set; }
+    private TimeSpan LevelTimer { get; set; }
     private Text _timerText;
+    public int LevelTime = 6;
 
 
-	// Use this for initialization
 	void Start () {
-        LevelTimer = TimeSpan.FromMinutes(6);
+        LevelTimer = TimeSpan.FromMinutes(LevelTime);
         _timerText = GetComponent<Text>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        LevelTimer = LevelTimer.Subtract(TimeSpan.FromSeconds(Time.deltaTime));
-        _timerText.text = LevelTimer.ToString();
-	}
+
+        GetLevelTimerObservable()
+            .Select(newTimer => newTimer.ToString())
+            .Do(newTimer => _timerText.text = newTimer)
+            .Subscribe();
+    }
+
+    public IObservable<TimeSpan> GetLevelTimerObservable()
+    {
+        return Observable.EveryUpdate()
+            .Select(_ => LevelTimer.Subtract(TimeSpan.FromSeconds(Time.deltaTime)))
+            .Do(newTimer => LevelTimer = newTimer)
+            .Share();
+    }
 }
